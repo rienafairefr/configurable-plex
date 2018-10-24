ff8f7c1694a37d76cffe36ac6427e66be73d4ebd
rienafairefr <rienafairefr@gmail.com>
2018-10-24 23:09:05 +0200

    try

--- root/etc/services.d/plex-setup/run.py
+++ root/etc/services.d/plex-setup/run.py
@@ -25,40 +25,40 @@ while True:
     except ConnectionError:
         time.sleep(1)
 
-acceptedEULA = plex.settings.get('acceptedEULA')
-acceptedEULA.set(True)
-plex.settings.save()
+try:
+    acceptedEULA = plex.settings.get('acceptedEULA')
+    acceptedEULA.set(True)
+    plex.settings.save()
 
-sections_to_add = yaml.load(open('/plex.conf.yml', 'r'))
-print(sections_to_add)
-
-
-def add_with_locations(title='', type='', agent='', scanner='', locations='', language='en', **kwargs):
-    locations = '&'.join('location=%s' % quote_plus(loc) for loc in locations)
-    part = '/library/sections?name=%s&type=%s&agent=%s&scanner=%s&language=%s&%s' % (
-        quote_plus(title), type, agent, quote_plus(scanner), language, locations)  # noqa E126
-    return plex.query(part, method=plex._session.post, **kwargs)
+    sections_to_add = yaml.load(open('/plex.conf.yml', 'r'))
+    print(sections_to_add)
 
 
-for section_name, section_to_add in sections_to_add.items():
-    section_to_add['title'] = section_name
-    found = False
-    print('to_add: ' + str(section_to_add.items()))
-    for section in plex.library.sections():
-        second = vars(section)
-        # section_to_add found completely inside secod
-        print('present: ' + str(list(second.items())))
-        if all(k in second and second[k] == v for k, v in section_to_add.items()):
-            # found the same section
-            found = True
-            print('Found section already added %s' % section_to_add)
-    print('---------------')
-    if not found:
-        print('Adding Section %s' % section_name)
-        add_with_locations(**section_to_add)
-    else:
-        print('Section %s already present' % section_name)
+    def add_with_locations(title='', type='', agent='', scanner='', locations='', language='en', **kwargs):
+        locations = '&'.join('location=%s' % quote_plus(loc) for loc in locations)
+        part = '/library/sections?name=%s&type=%s&agent=%s&scanner=%s&language=%s&%s' % (
+            quote_plus(title), type, agent, quote_plus(scanner), language, locations)  # noqa E126
+        return plex.query(part, method=plex._session.post, **kwargs)
 
 
-
-
+    for section_name, section_to_add in sections_to_add.items():
+        section_to_add['title'] = section_name
+        found = False
+        print('to_add: ' + str(section_to_add.items()))
+        for section in plex.library.sections():
+            second = vars(section)
+            # section_to_add found completely inside secod
+            print('present: ' + str(list(second.items())))
+            if all(k in second and second[k] == v for k, v in section_to_add.items()):
+                # found the same section
+                found = True
+                print('Found section already added %s' % section_to_add)
+        print('---------------')
+        if not found:
+            print('Adding Section %s' % section_name)
+            add_with_locations(**section_to_add)
+        else:
+            print('Section %s already present' % section_name)
+except:
+    # an error ocurred during the setup
+    pass
